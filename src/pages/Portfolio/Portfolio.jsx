542a5a8decb9daf7c81b8c4c5da199dd923c17b6
Pappyjay23 <pjinadu02@gmail.com>
2022-07-31 22:19:44 +0100

    Added styling for subcards

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import { CardContainer, PortfolioCard, PortfolioCardContainer, PortfolioContainer } from './Portfolio.styled'
-import SectionHeading from '../../components/SectionHeading/SectionHeading'
-import { PortfolioData } from '../../data/PortfolioData'
+import React from "react";
+import {
+	CardContainer,
+	PortfolioCard,
+	PortfolioCardContainer,
+	PortfolioContainer,
+	PortfolioSubCard,
+	PSubCardContent,
+	PSubCardDesc,
+	PSubCardTitle,
+} from "./Portfolio.styled";
+import SectionHeading from "../../components/SectionHeading/SectionHeading";
+import { PortfolioData } from "../../data/PortfolioData";
 
 const Portfolio = () => {
-  return (
-    <PortfolioContainer id='portfolio'>
-      <SectionHeading spanOne='Our Recent' spanTwo='Projects' spanThree='& Case Studies' spanFour="for clients" subTitle='Our portfolio' />
-      <PortfolioCardContainer>
+	return (
+		<PortfolioContainer id='portfolio'>
+			<SectionHeading
+				spanOne='Our Recent'
+				spanTwo='Projects'
+				spanThree='& Case Studies'
+				spanFour='for clients'
+				subTitle='Our portfolio'
+			/>
+			<PortfolioCardContainer>
 				<CardContainer>
 					{PortfolioData.map((data, index) => (
 						<PortfolioCard key={index} image={data.image}>
-							
+							<PortfolioSubCard>
+								<PSubCardContent>
+									<PSubCardTitle>{data.title}</PSubCardTitle>
+									<PSubCardDesc>{data.desc}</PSubCardDesc>
+								</PSubCardContent>
+							</PortfolioSubCard>
 						</PortfolioCard>
 					))}
 				</CardContainer>
 			</PortfolioCardContainer>
-    </PortfolioContainer>
-  )
-}
+		</PortfolioContainer>
+	);
+};
 
-export default Portfolio
+export default Portfolio;
--- src/pages/Portfolio/Portfolio.styled.js
+++ src/pages/Portfolio/Portfolio.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import SubCardBg from "../../assets/images/hover-bg.png";
 
 export const PortfolioContainer = styled.div`
 	padding: 3rem 15px;
@@ -19,15 +20,15 @@ export const PortfolioCardContainer = styled.div`
 	width: 95%;
 	margin: 4rem auto 0 auto;
 	overflow-x: scroll;
-	&::-webkit-scrollbar{
+	&::-webkit-scrollbar {
 		display: none;
 	}
 `;
 export const CardContainer = styled.div`
 	display: flex;
-`
+`;
 export const PortfolioCard = styled.div`
-	background-image: url(${({image}) => image});
+	background-image: url(${({ image }) => image});
 	background-position: center;
 	background-size: cover;
 	box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
@@ -36,8 +37,10 @@ export const PortfolioCard = styled.div`
 	margin-right: 2rem;
 	padding: 2rem;
 	border-radius: 15px;
+	position: relative;
+	transition: all 0.3s ease;
 
-	:last-child{
+	:last-child {
 		margin-right: 0;
 	}
 	@media screen and (min-width: 375px) {
@@ -55,4 +58,33 @@ export const PortfolioCard = styled.div`
 		width: 270px;
 		height: 350px;
 	}
+`;
+
+export const PortfolioSubCard = styled.div`
+	position: absolute;
+	bottom: 10px;
+	right: 10px;
+	background-image: url(${SubCardBg});
+	background-size: cover;
+	background-position: center;
+	height: 200px;
+	width: 210px;
+	border-top-left-radius: 30px;
+	border-bottom-right-radius: 30px;
+	padding: 1rem;
+	display: flex;
+	justify-content: flex-end;
+	align-items: flex-end;
+`;
+export const PSubCardContent = styled.div`
+	padding: 1rem 0;
+	text-align: center;
+	color: ${({ theme }) => theme.colors.white};
+`;
+export const PSubCardTitle = styled.p`
+	font-weight: 700;
+	font-size: 1.2rem;
+`;
+export const PSubCardDesc = styled.p`
+	font-size: 90%;
 `;
