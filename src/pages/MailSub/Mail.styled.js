1e447822787e02723d37d970b05440c359314e56
Pappyjay23 <pjinadu02@gmail.com>
2022-08-04 22:51:13 +0100

    Added media queries to the Mail section

--- src/pages/MailSub/Mail.styled.js
+++ src/pages/MailSub/Mail.styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import MailBg from '../../assets/images/subscribe-bg.png'
+import MailBg from "../../assets/images/subscribe-bg.png";
 
 export const MailContainer = styled.div`
 	padding: 5rem 15px;
@@ -17,17 +17,46 @@ export const MailContainer = styled.div`
 
 export const MailCardContainer = styled.div`
 	background-image: url(${MailBg});
-    background-position: center;
-    background-size: cover;
-	padding: 2rem;
+	background-position: center;
+	background-size: cover;
+	padding: 4rem 2rem;
 	border-radius: 20px;
+
+    @media (min-width: 768px){
+        padding: 4rem;
+    }
 `;
+export const MailBgd = styled.img`
+    position: absolute;
+    top: 10px;
+    right: 30px;
+    display: none;
+
+    @media (min-width: 1024px){
+        display: flex;
+    }
+`
 export const MailTitle = styled.p`
 	color: ${({ theme }) => theme.colors.white};
 	font-size: 1.8rem;
 	font-weight: 700;
 	text-align: center;
 	margin-bottom: 2rem;
+`;
+export const CardContentContainer = styled.div`
+	@media screen and (min-width: 768px) {
+		display: none;
+	}
+`;
+export const CardRoundedContainer = styled.div`
+	display: none;
+	@media screen and (min-width: 768px) {
+		background-color: ${({ theme }) => theme.colors.white};
+		padding: 1rem 1.5rem;
+		display: flex;
+		justify-content: space-between;
+		border-radius: 70px;
+	}
 `;
 export const WebContainer = styled.div`
 	background-color: ${({ theme }) => theme.colors.white};
@@ -39,6 +68,10 @@ export const WebInput = styled.input`
 	background-color: transparent;
 	border: none;
 	outline: none;
+
+	::placeholder {
+		color: #b2b2b2;
+	}
 `;
 export const EmailContainer = styled.div`
 	background-color: ${({ theme }) => theme.colors.white};
@@ -50,9 +83,13 @@ export const EmailInput = styled.input`
 	background-color: transparent;
 	border: none;
 	outline: none;
+
+	::placeholder {
+		color: #b2b2b2;
+	}
 `;
 export const ButtonContainer = styled.div`
-    margin-top: 1rem;
-    display: flex;
-    justify-content: center;
-`
+	margin-top: 1rem;
+	display: flex;
+	justify-content: center;
+`;
--- src/pages/MailSub/MailSub.jsx
+++ src/pages/MailSub/MailSub.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import { Button } from "../../components/Button/Button.styled";
 import {
-    ButtonContainer,
+	ButtonContainer,
+	CardContentContainer,
+	CardRoundedContainer,
 	EmailContainer,
 	EmailInput,
+	MailBgd,
 	MailCardContainer,
 	MailContainer,
 	MailTitle,
 	WebContainer,
 	WebInput,
 } from "./Mail.styled";
+import MailBgTwo from '../../assets/images/subscribe-dec.png'
 
 const MailSub = () => {
 	return (
 		<MailContainer>
 			<MailCardContainer>
+                <MailBgd src={MailBgTwo} />
 				<MailTitle>Know Your Website SEO Score by Email</MailTitle>
-				<WebContainer>
+				<CardContentContainer>
+					<WebContainer>
+						<WebInput placeholder='Your Website URL' type='text' />
+					</WebContainer>
+					<EmailContainer>
+						<EmailInput placeholder='Your Email' type='email' />
+					</EmailContainer>
+					<ButtonContainer>
+						<Button color='#fff' bg='#ff695f'>
+							Subscribe
+						</Button>
+					</ButtonContainer>
+				</CardContentContainer>
+				<CardRoundedContainer>
 					<WebInput placeholder='Your Website URL' type='text' />
-				</WebContainer>
-				<EmailContainer>
 					<EmailInput placeholder='Your Email' type='email' />
-				</EmailContainer>
-				<ButtonContainer>
-					<Button color='#fff' bg='#ff695f'>
+					<Button color='#fff' bg='#03a4ed'>
 						Subscribe
 					</Button>
-				</ButtonContainer>
+				</CardRoundedContainer>
 			</MailCardContainer>
 		</MailContainer>
 	);
