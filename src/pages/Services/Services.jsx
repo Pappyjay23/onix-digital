efa586d9ccb62509b3aba37716ccd8a68bf55d90
Pappyjay23 <pjinadu02@gmail.com>
2022-08-16 19:33:24 +0100

    Completed services section

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SectionHeading from "../../components/SectionHeading/SectionHeading";
 import {
 	CardContainer,
@@ -17,19 +17,6 @@ import { ServiceCardData } from "../../data/ServiceCardData";
 import { SectionContainer } from "../../components/SectionContainer/SectionContainer.styled";
 
 const Services = () => {
-	const [marqueeWidth, setMarqueeWidth] = useState("80vw");
-	const [elDisplayed, setElDisplayed] = useState(3);
-
-	useEffect(() => {
-		if (window.innerWidth >= 320) {
-			setElDisplayed(1)
-			setMarqueeWidth('90vw')
-		}else if (window.innerWidth >= 425) {
-			setElDisplayed(2)
-			setMarqueeWidth('80vw')
-		}
-	}, []);
-
 	return (
 		<SectionContainer id='services'>
 			<ServicesLeftBg src={LeftBg} />
@@ -43,17 +30,14 @@ const Services = () => {
 					subTitle='our services'
 				/>
 			</HeadingContainer>
-			<ServicesCardContainer
-				marqueeWidth={marqueeWidth}>
-				<CardContainer
-					marqueeWidth={marqueeWidth}
-					elDisplayed={elDisplayed}>
+			<ServicesCardContainer>
+				<CardContainer>
 					{ServiceCardData.map((data, index) => (
-						<ServicesCard
-							key={index}
-							marqueeWidth={marqueeWidth}>
+						<ServicesCard key={index}>
 							<ServicesCardTitle>{data.title}</ServicesCardTitle>
-							<ServicesCardIcon>{data.icon}</ServicesCardIcon>
+							<ServicesCardIcon>
+								{data.icon}
+							</ServicesCardIcon>
 							<ServicesCardDesc>{data.desc}</ServicesCardDesc>
 						</ServicesCard>
 					))}
